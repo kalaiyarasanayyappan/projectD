2ea929073413ef560410876ffa631d33130046c0
kalaiyarasan <kalaiyarasan.ayyappan@chainsys.com>
2022-07-11 17:49:21 +0530

    adding files

---
+++ src/com/chainsys/application/ProjectDApplication.java
@@ -0,0 +1,11 @@
+package com.chainsys.application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication 
+ class ProjectDApplication {
+           public static void main(String[] args) {
+               SpringApplication.run(ProjectDApplication.class, args);
+        }
+}
---
+++ src/com/chainsys/application/customer/CustomerService.java
@@ -0,0 +1,30 @@
+package com.chainsys.application.customer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+@RestController
+public class CustomerService {
+    @GetMapping("/findcustomer")
+    public String getCustomer() {
+        return "hello customer";
+    }
+
+    @PostMapping("/addcustomer")
+    public int addNewCustomer() {
+        return 0;
+    }
+    @PutMapping("/updatecustomer")
+    public int updateCustomer()
+    {
+        return 0;
+    }
+    @DeleteMapping("/deletecustomer")
+    public int deleteCustomer()
+    {
+        return 0;
+    }
+}
